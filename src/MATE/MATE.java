5223c3c06e824efcd3c53016aacaa3f386a8b7d7
ward46067 <ward46067@sas.edu.sg>
2016-04-21 08:55:44 +0800

    Initial Upload

---
+++ src/MATE/MATE.java
@@ -0,0 +1,14 @@
+package MATE;
+
+import com.ward.Console;
+
+public class MATE {
+    public static void main(String[] args) {
+        Console console = new Console();
+        console.setTitle("MATE Control");
+        console.setSize(400, 500);
+        console.build();
+        
+    }
+
+}
